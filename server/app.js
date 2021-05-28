9f3a093d0f7214d2b51a072d6136e767a75feabb
Paulo Enrique Lino de Siqueira <plino@br.ibm.com>
2021-05-28 18:39:25 -0300

    change to server

--- server/app.js
+++ server/app.js
@@ -4,8 +4,8 @@ const FacebookController = require('./services/facebook/controller')(server,app)
 
 const PORT = process.env.PORT || 1607;
 
-app.listen(PORT,()=>{
-    console.log('Running at 1607')
-})
+// app.listen(PORT,()=>{
+//     console.log('Running at 1607')
+// })
 
 server.listen(8080);
